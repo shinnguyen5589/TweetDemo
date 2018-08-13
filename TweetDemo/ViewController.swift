603629c724ff9626b94ee5b513150d84fe2ed496
Dung Nguyen <nhdung@fossil.com>
2018-08-13 20:36:48 +0700

    Initial Commit

---
+++ TweetDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TweetDemo
+//
+//  Created by Dung Nguyen Hoang on 8/13/18.
+//  Copyright © 2018 Fossil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

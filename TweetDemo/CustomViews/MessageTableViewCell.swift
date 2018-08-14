785d9b1132c155af3f8e50408ce81d35f211b718
Dung Nguyen <nhdung@fossil.com>
2018-08-14 11:02:35 +0700

    Add totalCharsLabel on message table view cell

--- TweetDemo/CustomViews/MessageTableViewCell.swift
+++ TweetDemo/CustomViews/MessageTableViewCell.swift
@@ -11,7 +11,7 @@ class MessageTableViewCell: BaseTableViewCell {
      override open class var CELL_IDENTIFIER: String { return "MessageTableViewCellID" }
     
     // Label to display message
-    private let _label: UILabel = {
+    private let _messageLabel: UILabel = {
         let view = UILabel()
         view.textColor = .black
         view.font = UIFont.systemFont(ofSize: 15)
@@ -20,24 +20,44 @@ class MessageTableViewCell: BaseTableViewCell {
         return view
     }()
     
+    private let _totalCharsLabel: UILabel = {
+        let view = UILabel()
+        view.textColor = .black
+        view.font = UIFont.systemFont(ofSize: 12)
+        view.numberOfLines = 0
+        view.lineBreakMode = NSLineBreakMode.byWordWrapping
+        return view
+    }()
+    
     override func setupViews() {
         self.backgroundColor = UIColor.white
-        self.contentView.addSubview(self._label)
+        self.contentView.addSubview(self._messageLabel)
+        self.contentView.addSubview(self._totalCharsLabel)
     }
     
     override func setupLayout() {
-        self._label.snp.remakeConstraints { (make) in
-            make.center.equalToSuperview()
-            make.leading.equalToSuperview().offset(20)
-            make.top.equalToSuperview().offset(10)
+        self._messageLabel.snp.remakeConstraints { (make) in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(15)
+            make.trailing.lessThanOrEqualTo(self._totalCharsLabel.snp.leading).offset(-5)
+            make.top.greaterThanOrEqualToSuperview().offset(2)
+        }
+        
+        self._totalCharsLabel.snp.remakeConstraints { (make) in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().offset(-10)
+            make.width.equalToSuperview().dividedBy(10)
+            make.top.greaterThanOrEqualToSuperview().offset(2)
         }
     }
     
     func configure(with message: String?) {
-        self._label.text = message
+        self._messageLabel.text = message
+        self._totalCharsLabel.text = "\(message?.count ?? 0)/\(MAX_TWEET_CHUNK_LENGTH)"
     }
     
     override func prepareForReuse() {
-        self._label.text = ""
+        self._messageLabel.text = ""
+        self._totalCharsLabel.text = ""
     }
 }
